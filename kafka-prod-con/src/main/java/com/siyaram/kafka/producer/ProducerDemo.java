8b10696ca7fadf9cb55d8a0ba13487f27f48ef47
siyaram <sonu.sgsits08@gmail.com>
2020-05-27 21:13:32 +0530

    initial commit skelton project

---
+++ kafka-prod-con/src/main/java/com/siyaram/kafka/producer/ProducerDemo.java
@@ -0,0 +1,13 @@
+package com.siyaram.kafka.producer;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProducerDemo 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
